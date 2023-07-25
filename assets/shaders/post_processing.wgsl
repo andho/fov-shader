6abd296746f89c8d83a018bbe4a91d6c65e9cb6c
Andho Mohamed <andhos@gmail.com>
2023-07-25 08:48:02 +0500

    initial commit

---
+++ assets/shaders/post_processing.wgsl
@@ -0,0 +1,48 @@
+// This shader computes the chromatic aberration effect
+
+#import bevy_pbr::utils
+
+// Since post processing is a fullscreen effect, we use the fullscreen vertex shader provided by bevy.
+// This will import a vertex shader that renders a single fullscreen triangle.
+//
+// A fullscreen triangle is a single triangle that covers the entire screen.
+// The box in the top left in that diagram is the screen. The 4 x are the corner of the screen
+//
+// Y axis
+//  1 |  x-----x......
+//  0 |  |  s  |  . ´
+// -1 |  x_____x´
+// -2 |  :  .´
+// -3 |  :´
+//    +---------------  X axis
+//      -1  0  1  2  3
+//
+// As you can see, the triangle ends up bigger than the screen.
+//
+// You don't need to worry about this too much since bevy will compute the correct UVs for you.
+#import bevy_core_pipeline::fullscreen_vertex_shader FullscreenVertexOutput
+
+@group(0) @binding(0)
+var screen_texture: texture_2d<f32>;
+@group(0) @binding(1)
+var screen_sampler: sampler;
+
+@group(1) @binding(0)
+var fov_texture: texture_2d<f32>;
+@group(1) @binding(1)
+var fov_sampler: sampler;
+
+@fragment
+fn fragment(in: FullscreenVertexOutput) -> @location(0) vec4<f32> {
+    // Chromatic aberration strength
+    let offset_strength = 0.0;
+
+    // Sample each color channel with an arbitrary shift
+    //return vec4<f32>(
+    //    textureSample(fov_texture, fov_sampler, in.uv).rgb,
+    //    0.2
+    //);
+    
+    return vec4<f32>(0.3, 0.2, 0.1, 0.0);
+}
+
---
+++ src/main.rs
@@ -0,0 +1,396 @@
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+use bevy::{
+    asset::ChangeWatcher,
+    core_pipeline::{
+        clear_color::ClearColorConfig, core_2d,
+        fullscreen_vertex_shader::fullscreen_shader_vertex_state,
+    },
+    prelude::*,
+    render::{
+        extract_component::{
+            ComponentUniforms, ExtractComponent, ExtractComponentPlugin, UniformComponentPlugin,
+        },
+        render_graph::{Node, NodeRunError, RenderGraphApp, RenderGraphContext},
+        render_resource::{
+            BindGroupDescriptor, BindGroupEntry, BindGroupLayout, BindGroupLayoutDescriptor,
+            BindGroupLayoutEntry, BindingResource, BindingType, CachedRenderPipelineId,
+            ColorTargetState, ColorWrites, FragmentState, MultisampleState, Operations,
+            PipelineCache, PrimitiveState, RenderPassColorAttachment, RenderPassDescriptor,
+            RenderPipelineDescriptor, Sampler, SamplerBindingType, SamplerDescriptor, ShaderStages,
+            ShaderType, TextureFormat, TextureSampleType, TextureViewDimension, Extent3d, TextureDescriptor, TextureDimension, TextureUsages, BlendState,
+        },
+        renderer::{RenderContext, RenderDevice},
+        texture::BevyDefault,
+        view::{ExtractedView, ViewTarget, RenderLayers},
+        RenderApp, extract_resource::{ExtractResource, ExtractResourcePlugin}, camera::RenderTarget, render_asset::RenderAssets,
+    },
+    utils::Duration, sprite::MaterialMesh2dBundle,
+};
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, FovPlugin))
+        .add_systems(Startup, (
+            vision_cone_texture_setup,
+            apply_deferred,
+            camera_setup.after(vision_cone_texture_setup),
+            fov_mesh_setup,
+            render_mesh,
+        ).chain())
+        .run();
+}
+
+// Entities with this component is rendered before main camera
+#[derive(Component)]
+struct FirstPassComponent;
+
+#[derive(Resource, Clone, Deref, ExtractResource)]
+struct FieldOfViewImage(Handle<Image>);
+
+fn vision_cone_texture_setup(
+    mut commands: Commands,
+    mut images: ResMut<Assets<Image>>,
+    mut _asset_server: Res<AssetServer>,
+) {
+    //let image_handle: Handle<Image> = asset_server.load("character/ball.png");
+
+    let size = Extent3d {
+        width: 1280,
+        height: 720,
+        ..default()
+    };
+
+    // This is the texture that will be rendered to.
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Rgba8UnormSrgb,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    // fill image.data with zeroes
+    image.resize(size);
+
+    let image_handle = images.add(image);
+
+    commands.insert_resource(FieldOfViewImage(image_handle))
+}
+
+#[derive(Component)]
+pub struct FovMarker;
+
+fn camera_setup(
+    mut commands: Commands,
+    fov_image: Res<FieldOfViewImage>,
+) {
+    let first_pass_layer = RenderLayers::layer(1);
+
+    commands.spawn((
+        Camera2dBundle {
+            camera_2d: Camera2d {
+                clear_color: ClearColorConfig::Custom(Color::BLACK),
+            },
+            camera: Camera {
+                // render before the "main pass" camera
+                order: -1,
+                target: RenderTarget::Image(fov_image.0.clone()),
+                ..default()
+            },
+            ..default()
+        },
+        first_pass_layer,
+    ));
+
+    commands.spawn((
+        Camera2dBundle {
+            ..default()
+        },
+        FovMarker,
+    ));
+}
+
+fn fov_mesh_setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let first_pass_layer = RenderLayers::layer(1);
+
+    // Circle
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+            ..default()
+        },
+        first_pass_layer,
+    ));
+}
+
+fn render_mesh(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    fov_image: Res<FieldOfViewImage>,
+) {
+
+    // Quad
+    //commands.spawn((
+    //    SpriteBundle {
+    //        texture: fov_image.0.clone(),
+    //        transform: Transform::from_translation(Vec3::new(-100., 1., 1.)),
+    //        ..Default::default()
+    //    },
+    //));
+
+    // Quad
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes
+            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
+            .into(),
+        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+        transform: Transform::from_translation(Vec3::new(50., 0., 2.)),
+        ..default()
+    });
+}
+
+struct FovPlugin;
+
+impl Plugin for FovPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((
+            ExtractResourcePlugin::<FieldOfViewImage>::default(),
+        ));
+
+        let Ok(render_app) = app.get_sub_app_mut(RenderApp) else {
+            return;
+        };
+
+        render_app
+            .add_render_graph_node::<PostProcessNode>(
+                core_2d::graph::NAME,
+                PostProcessNode::NAME,
+            )
+            .add_render_graph_edges(
+                core_2d::graph::NAME,
+                &[
+                    core_2d::graph::node::TONEMAPPING,
+                    PostProcessNode::NAME,
+                    core_2d::graph::node::END_MAIN_PASS_POST_PROCESSING,
+                ],
+            );
+    }
+
+    fn finish(&self, app: &mut App) {
+        let Ok(render_app) = app.get_sub_app_mut(RenderApp) else {
+            return;
+        };
+
+        render_app
+            .init_resource::<PostProcessPipeline>();
+    }
+}
+
+struct PostProcessNode {
+    query: QueryState<&'static ViewTarget, (With<ExtractedView>, With<FovMarker>)>,
+}
+
+impl PostProcessNode {
+    pub const NAME: &str = "post_process";
+}
+
+impl FromWorld for PostProcessNode {
+    fn from_world(world: &mut World) -> Self {
+        Self {
+            query: QueryState::new(world),
+        }
+    }
+}
+
+impl Node for PostProcessNode {
+    fn update(&mut self, world: &mut World) {
+        self.query.update_archetypes(world);
+    }
+
+    fn run(
+        &self,
+        graph_context: &mut RenderGraphContext,
+        render_context: &mut RenderContext,
+        world: &World,
+    ) -> Result<(), NodeRunError> {
+        let view_entity = graph_context.view_entity();
+        //let fov_image_handle = world.resource::<FieldOfViewImage>().0.clone();
+        //let gpu_images = world.resource::<RenderAssets<Image>>();
+        //let fov_image = &gpu_images[&fov_image_handle];
+
+        let Ok(view_target) = self.query.get_manual(world, view_entity) else {
+            return Ok(());
+        };
+
+        let post_process_pipeline = world.resource::<PostProcessPipeline>();
+
+        let pipeline_cache = world.resource::<PipelineCache>();
+
+        let Some(pipeline) = pipeline_cache.get_render_pipeline(post_process_pipeline.pipeline_id) else {
+            return Ok(());
+        };
+
+        let post_process = view_target.post_process_write();
+
+        let bind_group = render_context
+            .render_device()
+            .create_bind_group(&BindGroupDescriptor {
+                label: Some("post_process_bind_group"),
+                layout: &post_process_pipeline.layout,
+                entries: &[
+                    BindGroupEntry {
+                        binding: 0,
+                        // Make sure to use the source view
+                        resource: BindingResource::TextureView(post_process.source),
+                    },
+                    BindGroupEntry {
+                        binding: 1,
+                        resource: BindingResource::Sampler(&post_process_pipeline.sampler),
+                    },
+                ],
+            });
+
+        //let fov_bind_group = render_context
+        //    .render_device()
+        //    .create_bind_group(&BindGroupDescriptor {
+        //        label: Some("post_process_bind_group"),
+        //        layout: &post_process_pipeline.fov_layout,
+        //        entries: &[
+        //            BindGroupEntry {
+        //                binding: 0,
+        //                resource: BindingResource::TextureView(&fov_image.texture_view),
+        //            },
+        //            BindGroupEntry {
+        //                binding: 1,
+        //                resource: BindingResource::Sampler(&fov_image.sampler),
+        //            },
+        //        ],
+        //    });
+
+        let mut render_pass = render_context.begin_tracked_render_pass(RenderPassDescriptor {
+            label: Some("post_process_pass"),
+            color_attachments: &[Some(RenderPassColorAttachment {
+                view: post_process.destination,
+                resolve_target: None,
+                ops: Operations::default(),
+            })],
+            depth_stencil_attachment: None,
+        });
+
+        render_pass.set_render_pipeline(pipeline);
+        render_pass.set_bind_group(0, &bind_group, &[]);
+        //render_pass.set_bind_group(1, &fov_bind_group, &[]);
+        render_pass.draw(0..3, 0..1);
+
+        Ok(())
+    }
+}
+
+#[derive(Resource)]
+struct PostProcessPipeline {
+    layout: BindGroupLayout,
+    //fov_layout: BindGroupLayout,
+    sampler: Sampler,
+    pipeline_id: CachedRenderPipelineId,
+}
+
+impl FromWorld for PostProcessPipeline {
+    fn from_world(world: &mut World) -> Self {
+        let render_device = world.resource::<RenderDevice>();
+
+        let layout = render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+            label: Some("post_process_bind_group_layout"),
+            entries: &[
+                BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Texture {
+                        sample_type: TextureSampleType::Float { filterable: true },
+                        view_dimension: TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Sampler(SamplerBindingType::Filtering),
+                    count: None,
+                },
+            ],
+        });
+
+        //let fov_layout = render_device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+        //    label: Some("fov_post_process_bind_group_layout"),
+        //    entries: &[
+        //        BindGroupLayoutEntry {
+        //            binding: 0,
+        //            visibility: ShaderStages::FRAGMENT,
+        //            ty: BindingType::Texture {
+        //                sample_type: TextureSampleType::Float { filterable: true },
+        //                view_dimension: TextureViewDimension::D2,
+        //                multisampled: false,
+        //            },
+        //            count: None,
+        //        },
+        //        BindGroupLayoutEntry {
+        //            binding: 1,
+        //            visibility: ShaderStages::FRAGMENT,
+        //            ty: BindingType::Sampler(SamplerBindingType::Filtering),
+        //            count: None,
+        //        },
+        //    ],
+        //});
+
+        let sampler = render_device.create_sampler(&SamplerDescriptor::default());
+
+        let shader = world
+            .resource::<AssetServer>()
+            .load("shaders/post_processing.wgsl");
+
+        let pipeline_id = world
+            .resource_mut::<PipelineCache>()
+            .queue_render_pipeline(RenderPipelineDescriptor {
+                label: Some("post_process_pipeline".into()),
+                layout: vec![layout.clone()],
+                vertex: fullscreen_shader_vertex_state(),
+                fragment: Some(FragmentState {
+                    shader,
+                    shader_defs: vec![],
+                    entry_point: "fragment".into(),
+                    targets: vec![Some(ColorTargetState {
+                        format: TextureFormat::bevy_default(),
+                        blend: None,
+                        write_mask: ColorWrites::ALL,
+                    })],
+                }),
+                primitive: PrimitiveState::default(),
+                depth_stencil: None,
+                multisample: MultisampleState::default(),
+                push_constant_ranges: vec![],
+            });
+
+        Self {
+            layout,
+            //fov_layout,
+            sampler,
+            pipeline_id,
+        }
+    }
+}
